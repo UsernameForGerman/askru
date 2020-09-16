fc23eb29424b830de9d0592f1cca96ee690372d6
German <polyakgerman@gmail.com>
2020-09-16 15:26:45 +0300

    [Polls] - add tests & docker

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # askru
 API for polls
+
+Run:
+1. docker-compose build
+2. docker-compose up
+3. Go to your docker container via <container_id> and createsuperuser there
+4. You can log in to admin system via credentials you entered
--- config/local.py
+++ config/local.py
@@ -3,7 +3,7 @@ import os
 from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '36se8)23iv7a-ic!1r76&(b1(k*ncd4h3t$2a*+c+j$k#lnvef'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG', 'True') else False
@@ -22,7 +22,7 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = '/var/askru/media/'
+STATIC_ROOT = '/var/askru/static/'
 
 
